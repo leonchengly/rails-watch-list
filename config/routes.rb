33db8d3e5bd88ef1da733e7e979a1e898cb692a1
Li Yang Cheng <chengliyang@gmail.com>
2022-08-06 15:49:07 +0800

    Challenges Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "lists#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
